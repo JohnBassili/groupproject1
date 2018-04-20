f3fa11d50a7364d4b8df48b544b4a39a97f91ec2
john <JohnBassili@yahoo.com>
2018-04-19 21:00:48 -0500

    final push omega mix project

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -6,7 +6,6 @@ var artist = "Grimes";
 var artistsReturned = [];
 var searchedArtist;
 var similarArtistsNames = [];
-
 var JQr = $.noConflict();
 //JEFF End Initial Variables
 //Last.FM ajax call for similar artist
@@ -18,11 +17,7 @@ var SimilarArtist = Class.create({
         this.image = image;
         this.videos = videos;
     },
-<<<<<<< HEAD
-    print: function (){
-=======
     print: function () {
->>>>>>> 5470c6154d5d9d453bff93315b78995e5ddae5b2
         console.log(this);
     },
 });
@@ -33,55 +28,12 @@ var Artist = Class.create({
     },
 });
 //JEFF: End Prototype things
-<<<<<<< HEAD
-//  JESSICA: Beginning of onclick Event
-    JQr(document).on('click', '#submitButton', function(){
-        console.log("button clicked");
-        artist = JQr('#userSearch').val().trim();
-        console.log(artist);
-        //JESSICA: End of onclick Event
-        //JEFF: LAST.FM AJAX CALL
-        JQr.ajax({
-            url: "http://ws.audioscrobbler.com/2.0/?",
-            data: {
-                method: "artist.getinfo",
-                autocorrect: 1,
-                artist: artist,
-                api_key: lastfmAPIKey,
-                format: "json",
-            },
-            method: "GET",
-        }).then(function (response) {
-            console.log(response);
-           similarArtistsNames = getSimilarArtists(response.artist.similar.artist);
-
-           getSimilarArtistElements(similarArtistsNames[0]);
-           renderAccordion(similarArtistsNames);
-        });
-    });
-    //JEFF: END LASTFM AJAX CALL
-function getSimilarArtistElements(artist){
-    JQr.ajax({
-         url: "http://ws.audioscrobbler.com/2.0/?",
-         data: {
-                method: "artist.getinfo",
-                artist: artist,
-                api_key: lastfmAPIKey,
-                format: "json",
-         },
-         method: "GET",
-        }).then(function (response) {
-        var lastFMResponse = response;          
-=======
-
 //JESSICA: Event Listener for onkey
 //Press Enter Key to submit user input
-
 //trying to capture user input on keyup when ENTER pressed on keyboard. Console log doesn't return user input yet         
 var newInput = JQr('#userSearch');
 // newInput.on('keyup', console.log);
 newInput.on('keyup', enter);
-
 function enter (e) {
     console.log(e);//
     // event.preventDefault();
@@ -89,9 +41,7 @@ function enter (e) {
         JQr('#submitButton').click(); // Submit form code
         }
     }
-
 console.log("Pressing Enter Submits User Input: " + newInput);
-
 //  JESSICA: Beginning of onclick Event
 JQr(document).on('click', '#submitButton', function () {
     console.log("button clicked haha");
@@ -111,22 +61,16 @@ JQr(document).on('click', '#submitButton', function () {
         method: "GET",
     }).then(function (response) {
         console.log(response);
-
         similarArtistsNames = getSimilarArtists(response.artist.similar.artist);
         renderAccordion(similarArtistsNames);
         getSimilarArtistElements(similarArtistsNames[0], JQr('#card-body0'));
     });
-
 });
-
 //JESSICA: new onlcick needs to cb getSimilarArtistElements (AJAX CAlL!) and renderInner function (Vid and Bio elements!)
-
 //JEFF: END LASTFM AJAX CALL
-
-
 function getSimilarArtistElements(artist, target) {
     JQr.ajax({
-        url: "http://ws.audioscrobbler.com/2.0/?",
+        url: "https://ws.audioscrobbler.com/2.0/?",
         data: {
             method: "artist.getinfo",
             artist: artist,
@@ -136,7 +80,6 @@ function getSimilarArtistElements(artist, target) {
         method: "GET",
     }).then(function (response) {
         var lastFMResponse = response;
->>>>>>> 5470c6154d5d9d453bff93315b78995e5ddae5b2
         JQr.ajax({
             url: 'https://www.googleapis.com/youtube/v3/search?',
             data: {
@@ -151,48 +94,19 @@ function getSimilarArtistElements(artist, target) {
             method: 'GET',
         }).then(function (response) {
             console.log(response);
-
-<<<<<<< HEAD
-
-        var simArtist = new SimilarArtist(artist, lastFMResponse.artist.bio.summary, getArtistImages(lastFMResponse), getVideoIds(response));
-        console.log(simArtist);
-        simArtist.print();
-        console.log("KILROY WAS HERE");
-
-
-//         var testVid = JQr("<iframe>");
-// testVid.addClass("embed-responsive-item");
-// testVid.attr("src","https://www.youtube.com/embed/" + simArtist.videos[0]);
-// JQr("#collapseFive").append(testVid);
-
-
-	});
-});
-    }
-=======
             var simArtist = new SimilarArtist(artist, lastFMResponse.artist.bio.summary, getArtistImages(lastFMResponse), getVideoIds(response));
             console.log(simArtist);
             simArtist.print();
             console.log("KILROY WAS HERE");
             // Here is where Jessica needs to cb the renderInner function
             renderInner(simArtist, target);
-
             //         var testVid = JQr("<iframe>");
             // testVid.addClass("embed-responsive-item");
             // testVid.attr("src","https://www.youtube.com/embed/" + simArtist.videos[0]);
             // JQr("#collapseFive").append(testVid);
-
         });
     });
 }
-
->>>>>>> 5470c6154d5d9d453bff93315b78995e5ddae5b2
-
-
-
-
-
-
 function getArtistImages(response) {
     var artistImages = []
     for (var i = 0; i < 5; i++) {
@@ -200,10 +114,6 @@ function getArtistImages(response) {
     }
     return artistImages[4];
 }
-
-
-
-
 //JEFF: MISC FUNCTIONS
 function getSimilarArtists(artistsArray) {
     artistsReturned = [];
@@ -217,9 +127,6 @@ function getSimilarArtists(artistsArray) {
     return artistsReturned;
 }
 //JEFF: END MISC FUNCTIONS
-
-
-
 //JEFF: YOUTUBE API FOR IDS
 function getVideoIds(response) {
     var videoids = [];
@@ -229,24 +136,13 @@ function getVideoIds(response) {
     console.log(videoids);
     return videoids;
 };
-
-<<<<<<< HEAD
-
-    //JEFF: MISC FUNCTIONS
-    function getSimilarArtists(artistsArray) {
-        artistsReturned = [];
-        for (var i = 0; i < artistsArray.length; i++) {
-            artistsReturned.push(artistsArray[i].name);
-=======
 //  JOHN BASSILI START OF RENDER ACCORDION AS A FUNCTION 
 function renderAccordion(similarArtists) {
     var newResultsDiv = JQr("<div>");
-
     newResultsDiv.addClass("accordion");
     // create a loop TO LOOP THROUGH EACH OF THE LINES IN FIRST DIV, THEN REPEAT THIS PROCESS FOR EACH DIV
     for (var i = 0; i < similarArtists.length; i++) {
         similarArtists[i]
-
         var newCard = JQr("<div>");
         newCard.addClass("card");
         newResultsDiv.append(newCard);
@@ -272,7 +168,6 @@ function renderAccordion(similarArtists) {
         }
         else {
             collapseOne.addClass("collapse");
->>>>>>> 5470c6154d5d9d453bff93315b78995e5ddae5b2
         }
         collapseOne.attr("data-parent", "accordion" + i);
         newCard.append(collapseOne);
@@ -282,114 +177,34 @@ function renderAccordion(similarArtists) {
         collapseOne.append(newcardbody);
         
     }
-<<<<<<< HEAD
-    //JEFF: END MISC FUNCTIONS
-    //JEFF: YOUTUBE API FOR IDS
-    function getVideoIds(response) {
-        var videoids = [];
-            for(var i=0;i<numYouTubeResultsPerArtist;i++){
-                videoids.push(response.items[i].id.videoId);
-            }
-            console.log(videoids);
-            return videoids;
-        };
-=======
     JQr('#searchResults').empty();
     JQr("#searchResults").append(newResultsDiv);
     //JEFF: END YOUTUBE API
 }
-
 // JESSICA & JEFF'S CODE: Adding band photo and elements dynamically
 //render content for each artist card
 function renderInner(similarObject, insertHere) {
     insertHere.empty(); //empty div
-
     //create divs for Row 1 fand Row 2 within each artist card.
->>>>>>> 5470c6154d5d9d453bff93315b78995e5ddae5b2
-
     //add firstRow for band image and bio
     var firstRow = JQr('<div>');
     firstRow.addClass('row');
     firstRow.attr('id', 'first-row');
-
-<<<<<<< HEAD
-  //  JOHN BASSILI START OF RENDER ACCORDION AS A FUNCTION 
-    function renderAccordion(similarArtists){
-
-      var newResultsDiv = JQr("<div>");
-    
-    newResultsDiv.addClass("accordion");
-
-    // create a loop TO LOOP THROUGH EACH OF THE LINES IN FIRST DIV, THEN REPEAT THIS PROCESS FOR EACH DIV
-
-    for (var i = 0; i < similarArtists.length; i++) {
-        similarArtists[i]
-
-    
-
-    var newCard = JQr("<div>");
-    newCard.addClass("card");
-    newResultsDiv.append(newCard);
-
-    var newcardheader = JQr("<div>");
-    newcardheader.addClass("card-header");
-    newcardheader.attr("id","heading"+i);
-    newCard.append(newcardheader);
-
-    var h5 = JQr("<h5>");
-    h5.addClass("mb-0")
-    newcardheader.append(h5);
-
-    var button = JQr("<button>");
-    button.addClass("btn btn-link");
-    button.attr("data-toggle","collapse");
-    button.attr("data-target", "#collapse"+i);
-    button.attr("data-index", i);
-    button.text(similarArtistsNames[i]);
-
-
-        h5.append(button);
-
-    var collapseOne =JQr("<div>");
-    collapseOne.attr("id", "collapse"+i);
-    if (i===0) {
-
-    collapseOne.addClass("collapse show");
-}
-else {
-    collapseOne.addClass("collapse");
-}
-    collapseOne.attr("data-parent","accordion"+i);
-    newCard.append(collapseOne);
-
-    var newcardbody = JQr("<div>");
-    newcardbody.addClass("card-body", "img-responsive");
-    // newcardbody.attr("");
-    collapseOne.append(newcardbody);
-}
-    JQr("#searchResults").append(newResultsDiv);
-//JEFF: END YOUTUBE API
-}
-=======
     //adding band image firstRow
     var bandImages = JQr('<div>');
     bandImages.addClass('col-md-4'); //formatting column space for band image via Bootstrap
     var bandPhoto = JQr('<img>'); // dynamically create attr for artist images
-
     bandPhoto.attr('src', similarObject.image); //img src attribute
     bandPhoto.addClass('band-image'); //add class
     bandImages.append(bandPhoto); //append bandPhoto to bandImages div
-
     //Now add band bio to firstRow
     var bandBio = JQr('<p>'); //new p tag for bank bio
     bandBio.addClass('col-md-8'); //formatting column space for band bio via Bootstrap
     bandBio.html(similarObject.bio + "</br>" + "<p><strong><i>content courtesy of Last.fm</i></strong></p>"); //copy the returned bio from object to html; noted to bio source
-
     // add video to artist acordion
     var secondRow = JQr('<div>');
     secondRow.addClass('row');
     secondRow.attr('id', 'second-row');
-
     for (var i = 0; i < similarObject.videos.length; i++) {
         var testVid = JQr("<iframe>");
         testVid.addClass("embed-responsive-item");
@@ -401,10 +216,10 @@ else {
     firstRow.append(bandImages, bandBio);
     insertHere.append(firstRow, secondRow); //cool! remember this, Jessica
 }
-
 JQr('#searchResults').on('click', '.recommendation-button', function () {
     var index = parseInt(JQr(this).attr("data-index")); //parse index from string to number
     console.log("index" + index);
     getSimilarArtistElements(similarArtistsNames[index], JQr('#card-body'+ index));
 });
->>>>>>> 5470c6154d5d9d453bff93315b78995e5ddae5b2
+
+
